c2d52a31d1789debc37d915c7b1775ab09962812
bairinc0 <bairinc0@gmail.com>
2013-09-25 21:12:56 +0900

    Глава 2

---
+++ java/JavaFilosophy/chapter2/Documentation1.java
@@ -0,0 +1,8 @@
+//:object/Documentation1.java
+/**Документация к классу Documentation1*/
+public class Documentation1{
+	/**Комментарий к переменной*/
+	public int i;
+	/**Комментарий к методу*/
+	public void f(){}
+}///:~
---
+++ java/JavaFilosophy/chapter2/HelloDate.java
@@ -0,0 +1,18 @@
+import java.util.*;
+/** Моя первая программа на Java
+* Выводит приветствие и текущее число
+*@author Profchaos
+*@author profchaos.ru
+*@version 2.0
+*/
+public class HelloDate{
+	/**
+	*Точка входа в приложение
+	*@param args Массив строковых аргументов
+	*@throws exceptions Исключения не выдаются
+	*/
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+		System.out.println(new Date());
+	}
+} 
